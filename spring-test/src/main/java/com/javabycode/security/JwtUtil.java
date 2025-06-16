bdbf4dd7bc14125c8a5c10f1b5546f1ced80062c
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2025-06-16 16:58:15 -0300

    feat: add generateTestToken method for JWT token creation

--- spring-test/src/main/java/com/javabycode/security/JwtUtil.java
+++ spring-test/src/main/java/com/javabycode/security/JwtUtil.java
@@ -4,9 +4,13 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.Claims;
+import org.springframework.stereotype.Component;
+
 import java.util.Date;
+import java.util.List;
 import javax.crypto.SecretKey;
 
+@Component
 public class JwtUtil {
     private static final SecretKey SECRET_KEY = Keys.hmacShaKeyFor("mysecretkeymysecretkeymysecretkey12".getBytes());
     private static final long EXPIRATION = 1000 * 60 * 60; // 1 hora
@@ -27,5 +31,19 @@ public class JwtUtil {
                 .parseClaimsJws(token)
                 .getBody();
         return claims.getSubject();
+    }
+
+    // 3 partes do token: header, payload, signature
+    // header: algoritmo de assinatura e tipo do token
+    // payload: informações do usuário (claims)
+    // signature: assinatura do token para verificar a autenticidade
+    public String generateTestToken(String username, List<String> roles) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("authorities", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
+                .compact();
     }
 }
